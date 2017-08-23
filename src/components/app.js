89d44aee927760c5a0ea9f4639b3d3b803a272c4
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-08-23 13:47:33 +0200

    install react-router-dom

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <img src={'http://s2.quickmeme.com/img/1c/1cba02b056944308e72401de0acbf9dbb471aad66deea493613a633be13ab92d.jpg'} alt="bender" />
-        <p>first shot!</p>
+        lalala
       </div>
     );
   }
