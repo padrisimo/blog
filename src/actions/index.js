4d4bf65dbd6bff71aa5a00428bc2ca04c71ef6d8
padrisimo <nopuedoparardemolar@outlook.com>
2017-08-27 13:47:35 +0200

    create post action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_post';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=h4zl0alfredolandanesco';
@@ -12,4 +13,13 @@ export function fetchPosts() {
         type: FETCH_POSTS,
         payload: request
     };
+}
+
+export function createPost(values){
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+    return {
+        type: CREATE_POST,
+        payload: request
+    }
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -22,8 +24,7 @@ class PostsNew extends Component {
     }
 
     onSubmit(values){
-        // this === component
-        console.log(values);
+        this.props.createPost(values);
     }
 
     render() {
@@ -73,4 +74,6 @@ const validate = (values) =>{
 export default reduxForm({
     validate,
     form: 'PostNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost } )(PostsNew)
+);
