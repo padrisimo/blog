3617c57d2619ca408dfde2378df345795ed29b3a
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-08-29 15:24:36 +0200

    deleting a post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=h4zl0alfredolandanesco';
@@ -34,3 +35,14 @@ export function fetchPost(id) {
         payload: request
     }
 }
+
+export const deletePost = (id) =>  {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+
+    return {
+        type: DELETE_POST,
+        payload: id
+    }
+};
+
